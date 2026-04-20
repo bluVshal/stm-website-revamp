a13d6b5d2148cfa2d37ada8797cdc95c9dd1611d
PraVishal <blu.vishal@gmail.com>
2026-04-20 15:00:18 +0400

    Added modifications: About Us details and Mission Purpose0

---
+++ app/about-us/components/aboutUsDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUsDetails = () => {
+  return (
+    <div><p className='p-6'>STM Consulting is a leading provider of tailored services for the iGaming industry. With a focus on innovation and client success, we offer a comprehensive suite of solutions including managed services, digital marketing, graphic design, content creation, consultation, and a robust iGaming platform.
+
+Our experienced team brings together industry expertise, cutting-edge technology, and a collaborative approach to help operators thrive in a competitive market. Whether it’s optimizing player acquisition, enhancing customer support, or driving brand visibility, STM Consulting delivers exceptional results, tailored to meet the unique needs of each client. With a proven track record, global reach, and a passion for excellence, we are the trusted partner to elevate your iGaming operations to new heights of success.</p></div>
+  )
+}
+
+export default AboutUsDetails
